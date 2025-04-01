099f79701d948edc75b37707e9e77150c47a94e6
drx <75536513+drxMalick123@users.noreply.github.com>
2025-04-01 08:12:58 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ import os
 from main import app
 
 if __name__ == "__main__":
+    print("Running Flask app...")
     port = int(os.environ.get("PORT", 4000))  # Default to 4000 if PORT is not set
     app.run(host='0.0.0.0', port=port, debug=True)
