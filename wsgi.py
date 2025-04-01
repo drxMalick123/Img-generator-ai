cc35f04b107f61b1543f738bce039a35f152c142
drx <75536513+drxMalick123@users.noreply.github.com>
2025-04-01 12:05:10 +0530

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-from main import app
-
-if __name__ == "__main__":
-    print("Running Flask app...")
-    port = int(os.environ.get("PORT", 4000))  # Get port from environment
-    app.run(host='0.0.0.0', port=port)
