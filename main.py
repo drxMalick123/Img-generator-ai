aae45026a7cc10eceb8f582d12a41c0da69fcc5e
drx <75536513+drxMalick123@users.noreply.github.com>
2025-04-01 11:31:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def generate():
 
 if __name__ == '__main__':
     # por = int(os.environ.get("PORT", 5000))  # Get PORT from environment
-    print(f"Starting Flask on port {por}...")
+    # print(f"Starting Flask on port {por}...")
     app.logger.info(f"check is ok ")  # Log request
   
     app.run(host='0.0.0.0', port=5000)
