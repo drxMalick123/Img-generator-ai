0b0e3b14774fd912b95bb16cde2e4ed769171bf0
drx <75536513+drxMalick123@users.noreply.github.com>
2025-04-01 07:14:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ def generate():
     return jsonify({"image_url": image_url})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=4000, debug=True)
