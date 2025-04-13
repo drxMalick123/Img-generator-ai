294781f37af01ee4bf68b39e7183b42bd3654b22
drx <75536513+drxMalick123@users.noreply.github.com>
2025-04-13 05:58:36 +0530

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ function generateImage() {
     let textInput = document.getElementById("textInput").value;
     let outputDiv = document.getElementById("output");
 
-    fetch("http://127.0.0.1:5000/generate", {
+    fetch("/generate", {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
